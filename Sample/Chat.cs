9a32b9272dbaf1a12034ba730e2772ab3ef9f0b0
David Fowler <davidfowl@gmail.com>
2017-09-09 08:11:13 -0700

    Add vs code launchability - Improved sample to show OnConnectedAsync and OnDisconnectedAsync methods.

--- Sample/Chat.cs
+++ Sample/Chat.cs
@@ -6,6 +6,16 @@ namespace Sample
 {
     public class Chat : Hub
     {
+        public  override Task OnConnectedAsync()
+        {
+            return Clients.All.InvokeAsync("Send", $"{Context.ConnectionId} joined");
+        }
+
+        public override Task OnDisconnectedAsync(Exception exception)
+        {
+            return Clients.All.InvokeAsync("Send", $"{Context.ConnectionId} left");
+        }
+
         public Task Send(string message)
         {
             return Clients.All.InvokeAsync("Send", message);
