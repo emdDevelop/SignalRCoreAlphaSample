686f4c242a284e60e1a8e71f53b4166c431a726f
David Fowler <davidfowl@gmail.com>
2017-09-13 01:10:11 -0700

    Added console example

---
+++ SampleConsole/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR.Client;
+using Microsoft.AspNetCore.Sockets;
+
+namespace SampleConsole
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var connection = new HubConnectionBuilder()
+                            .WithUrl("http://localhost:5000/chat")
+                            .WithMessagePackProtocol()
+                            .WithConsoleLogger()
+                            .WithTransportType(TransportType.WebSockets)
+                            .Build();
+
+            await connection.StartAsync();
+            var cts = new CancellationTokenSource();
+
+            Console.CancelKeyPress += (sender, e) =>
+            {
+                e.Cancel = true;
+                cts.Cancel();
+            };
+
+            connection.Closed += e =>
+            {
+                cts.Cancel();
+                return Task.CompletedTask;
+            };
+
+            connection.On<string>("Send", data =>
+            {
+                Console.WriteLine($"Server: {data}");
+            });
+
+            var cancelledTask = Task.Delay(-1, cts.Token);
+
+            while (!cts.IsCancellationRequested)
+            {
+                Console.Write("Client: ");
+                var task = await Task.WhenAny(cancelledTask, Task.Run(() => Console.ReadLine()));
+                if (task is Task<string> readline)
+                {
+                    var line = await readline;
+
+                    await connection.InvokeAsync("Send", line);
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            await connection.DisposeAsync();
+
+            cts.Dispose();
+        }
+    }
+}
